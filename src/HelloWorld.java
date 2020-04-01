56fd3874744e5978a812f70282fbfdd65ffba283
SoapEye <lious.q.k@gmail.com>
2020-04-01 10:31:12 +0800

    2

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+//class 类  HelloWorld 是类名 public class 是告诉java类名要与代码文件名一致
+//大括号内是类的内容
+//public static void main (String[] args) --main方法,告诉这是程序入口,程序开始执行的地方
+//{}括号是方法的内容,又称方法体(method body)
+//System.out.printlns是java内置功能,可以将内容输出. ()小括号里面是参数,无则输出一行空行.
+public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+    }
+}
