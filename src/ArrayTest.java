50a17931a71fc32d02e1b0dd412e696534302815
SoapEye <lious.q.k@gmail.com>
2020-04-17 08:38:14 +0800

    20200417

---
+++ src/ArrayTest.java
@@ -0,0 +1,9 @@
+public class ArrayTest {
+    public static  void main(String[] args) {
+        //数组方式定义同学成绩
+        int [] ns = new int[5];
+        ns[1] = 79;
+        ns[2] = 43;
+        ns[0] = 99;
+    }
+}
---
+++ src/Zifuc.java
@@ -0,0 +1,8 @@
+public class Zifuc {
+    public static void main (String[] args) {
+         String s1 = "Hello";
+         String s2 = "world";
+         String s = s1 + "" + s2 + "!";
+         System.out.println(s);
+    }
+}
