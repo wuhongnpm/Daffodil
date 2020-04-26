5301fade10544692249612909aaea5ef6165670e
SoapEye <lious.q.k@gmail.com>
2020-04-26 10:03:45 +0800

    12313

---
+++ src/Print.java
@@ -0,0 +1,11 @@
+public class Print {
+    public  static void main (String[] args) {
+        int a = 100;
+        if (a > 100) {
+            System.out.println("及格了");
+        }else {
+            System.out.println("挂科了");
+        }
+        System.out.print("MONO");
+    }
+}
--- src/print.java
+++
@@ -1,7 +0,0 @@
-public class print {
-    public static void main(String[] args) {
-        double d = 3.1415926;
-        System.out.printf("%.2f\n", d); // 显示两位小数3.14
-        System.out.printf("%.4f\n", d); // 显示4位小数3.1416
-    }
-}
