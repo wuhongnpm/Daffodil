7fe1a29476c0a8be33341503417bf46445a5a2e6
SoapEye <lious.q.k@gmail.com>
2020-05-08 17:47:27 +0800

    123123

--- src/Pdemoa.java
+++ src/Pdemoa.java
@@ -1,6 +1,8 @@
 //javac 后面跟文件名 java后面根文件中的类名
 public class Pdemoa {
     public static  void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!!!");
+
     }
+
 }
---
+++ src/Pdemoh.java
@@ -0,0 +1,12 @@
+public class Pdemoh {
+    public static void main(String[] args) {
+        FreshJuice juice = new FreshJuice();
+        juice.size = FreshJuice.FreshJuiceSize.MEDIUM;
+    }
+    static class FreshJuice {
+        enum FreshJuiceSize{ SMALL,MEDIUM, LARGE}
+        FreshJuiceSize size;
+    }
+}
+
+
