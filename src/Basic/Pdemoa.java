079d668e627040f11dc1b07f368d0c8984b845b1
SoapEye <lious.q.k@gmail.com>
2020-07-07 09:55:57 +0800

    20200707

--- src/Basic/Pdemoa.java
+++ src/Basic/Pdemoa.java
@@ -4,6 +4,10 @@ package Basic;
 public class Pdemoa {
     public static  void main(String[] args) {
         System.out.println("Hello World!!!");
+        int z = 100;
+        final  double PI = 3.14926;
+        System.out.println(z);
+        System.out.println(PI);
 
     }
 
