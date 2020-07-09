97c93d38a10dfb545d0c4561792dc894d0327bde
SoapEye <lious.q.k@gmail.com>
2020-07-09 17:08:02 +0800

    2

--- src/Basic/Pdemog.java
+++ src/Basic/Pdemog.java
@@ -1,6 +1,15 @@
 package Basic;
 
 public class Pdemog {
-    
-
+    public static void main(String[] args) {
+        int [] a1 = {1,2,3};
+        int [] a2;
+        a2 = a1;
+        for(int i = 0; i < a2.length; i++){
+            a2[i]++;
+        }
+        for(int i = 0; i < a1.length; i++){
+            System.out.println(a1[i]);
+        }
+    }
 }
