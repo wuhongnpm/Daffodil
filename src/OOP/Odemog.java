b807927a2671e26810e08f4d5dadd3168d599310
SoapEye <lious.q.k@gmail.com>
2020-07-21 08:34:12 +0800

    2020

---
+++ src/OOP/Odemog.java
@@ -0,0 +1,32 @@
+package OOP;
+
+public class Odemog {
+    void f(int i) {
+        System.out.println("i=" + i);
+    }
+
+    void f(float f) {
+        System.out.println("f=" + f);
+    }
+
+    void f(String s) {
+        System.out.println("s=" + s);
+    }
+
+    void f(String s1, String s2){
+        System.out.println("s1+s2="+(s1+s2));
+    }
+
+    void f(String s, int i){
+        System.out.println("s="+s+",i="+i);
+    }
+
+    public static void main(String[] args) {
+        Odemog test = new Odemog();
+        test.f(3456);
+        test.f(34.56f);
+        test.f("abc");
+        test.f("abc","def");
+        test.f("abc",3456);
+    }
+}
