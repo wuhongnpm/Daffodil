87c589ebf6db151c83a853c6f298515c84017c56
SoapEye <lious.q.k@gmail.com>
2020-08-13 09:33:57 +0800

    八月第二周周四

--- src/Else/JDBCmysql.java
+++ src/Else/JDBCmysql.java
@@ -1,6 +1,13 @@
-package Else;
 
 /**
+package Else;
+ */
+
+/*
+
+
+import java.sql.DriverManager;
+
 public class JDBCmysql {
     //注册mysql驱动程序
     try {
@@ -10,9 +17,37 @@ public class JDBCmysql {
         System.out.println("Error:unable to load driver class!");
         System.exit(1);
     }
-    Driver driver = new com.mysql.jdbc.Driver();
+*/
+/*
+    //指定数据库连接URL
+    Driver driver = new
+    com.mysql.jdbc.Driver();
     DriverManager.registerDriver(driver);
+
+    String URL = "";
+    String USER= ""
+            Connection conn =
+                    DriverManager.getConnection(URL);
+*/
+
+    //使用数据库的URL
+
+/**
+
+   import java.util.*;
+
+   String URL = "jdbc:mysql://localhost/EXAMPLE";
+   Properties Pro = new Properties();
+   //Propertties对象，保存一组关键字-值对
+           pro.put("user","root");
+           pro.put("password","");
+           Connection conn =
+           DriverManager.getConnection(URL,Pro);
+           conn.close();
+
     
 
 }
- **/
+*/
+
+
