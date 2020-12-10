8af1ad473d7fa19ccfb8e8a531a96781305f6a6c
SoapEye <lious.q.k@gmail.com>
2020-12-10 17:54:19 +0800

    1

--- src/twtc/Start.java
+++ src/twtc/Start.java
@@ -1,7 +1,17 @@
 package twtc;
 
 public class Start {
+    String name;
+    float  age;
+    int number;
+    void hit() {
+        System.out.println("KKOO");
+    }
     public static void main (String[] args) {  //主方法
+        Start mike = new Start();
+        mike.name = "Jim.Mike";
+        mike.age = 21;
+        mike.number = 001;
         System.out.println("Hello World"); //输出
     }
 }
