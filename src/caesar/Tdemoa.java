929cdfcbd9b4d239404729334e472983f7c8155c
SoapEye <lious.q.k@gmail.com>
2021-02-25 09:16:32 +0800

    0225

---
+++ src/caesar/Tdemoa.java
@@ -0,0 +1,18 @@
+package caesar;
+
+public class Tdemoa {
+    //常量
+    int x = 100;
+    final double PI = 3.1415926
+    public static void main (String[] args) {
+        System.out.println("KK id Fine");
+    }
+    /*多行注释*/
+    //单行注释
+    /***
+     * 多行注释1
+     * 多行注释2
+     */
+
+
+}
