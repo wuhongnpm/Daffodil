0305928ded66bf11485602b6609b19cf69786a10
SoapEye <lious.q.k@gmail.com>
2021-04-30 15:51:48 +0800

    2321

--- src/April/Demog.java
+++ src/April/Demog.java
@@ -2,6 +2,15 @@ package April;
 
 public class Demog {
     public static void main(String[] args) {
-        System.out.println("Hello Wolrd");
+       Demog tester = new Demog();
+
+       //带有类型声明的表达式
+        MathOperation addition = (int a, int b) -> a + b;
+        //没有类型声明的表达式
+        MathOperation subtraction = (a,b) -> a -b;
+        //带有大括号,带有返回语句的表达式
+        MathOperation multiplication = (int a, int b) ->{ return a * b;};
+
+
     }
 }
