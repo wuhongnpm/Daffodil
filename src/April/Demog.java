2c5dd6e9fec581c0f8c7a3ae87d918b15f9eb87c
SoapEye <lious.q.k@gmail.com>
2021-05-11 09:41:02 +0800

    2

--- src/April/Demog.java
+++ src/April/Demog.java
@@ -3,7 +3,7 @@ package April;
 public class Demog {
     public static void main(String[] args) {
        Demog tester = new Demog();
-
+/*
        //带有类型声明的表达式
         MathOperation addition = (int a, int b) -> a + b;
         //没有类型声明的表达式
@@ -11,6 +11,6 @@ public class Demog {
         //带有大括号,带有返回语句的表达式
         MathOperation multiplication = (int a, int b) ->{ return a * b;};
 
-
+*/
     }
 }
---
+++ src/May/Demo.java
@@ -0,0 +1,7 @@
+package May;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("CCL");
+    }
+}
