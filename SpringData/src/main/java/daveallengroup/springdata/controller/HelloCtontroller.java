6987cc4dd0b5b9e93c5ad837ef2776c74aac1483
Daveallen <lious.q.k@gmail.com>
2022-08-26 10:11:52 +0800

    2
    2

---
+++ SpringData/src/main/java/daveallengroup/springdata/controller/HelloCtontroller.java
@@ -0,0 +1,15 @@
+package daveallengroup.springdata.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+//@RestController注释相当于@ResponseBody +@Controller合在一起
+@RestController
+public class HelloCtontroller {
+    @RequestMapping("/hello")
+    public String hello(String name){
+        return "Can you Felling"+name;
+    }
+}
